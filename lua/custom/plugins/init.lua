bc4425e64c97055f767db3def170fd0745f138d5
Paolo Gabriel Wacker <pwacker@3-E-1.42heilbronn.de>
2024-11-15 14:14:28 +0100

    1.2

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  --  42-header plugin
   {
     'Diogo-ss/42-header.nvim',
     cmd = { 'Stdheader' },
@@ -17,63 +18,7 @@ return {
       require('42header').setup(opts)
     end,
   },
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      vim.opt.termguicolors = true
-      vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
-      require('nvim-tree').setup {
-        view = {
-          width = 30,
-          side = 'left',
-        },
-        git = {
-          enable = true,
-          ignore = false,
-          timeout = 500,
-        },
-        renderer = {
-          highlight_git = true,
-          group_empty = true,
-          icons = {
-            show = {
-              git = true,
-            },
-            glyphs = {
-              default = '→',
-              symlink = '⤴',
-              git = {
-                unstaged = '✗',
-                staged = '✓',
-                unmerged = '≠',
-                renamed = '➜',
-                untracked = '★',
-                deleted = '🗑',
-                ignored = '◌',
-              },
-              folder = {
-                arrow_open = '▾',
-                arrow_closed = '▸',
-                default = '📁',
-                open = '📂',
-                empty = '🗃',
-                empty_open = '🗃',
-                symlink = '🔗',
-                symlink_open = '🔗',
-              },
-            },
-          },
-        },
-        filters = {
-          dotfiles = true,
-        },
-      }
-    end,
-  },
+  --  norminette plugin
   {
     'hardyrafael17/norminette42.nvim',
     config = function()
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
@@ -14,12 +14,47 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    enable_git_status = true,
     filesystem = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['o'] = 'system_open',
         },
       },
+      renderers = {
+        directory = {
+          { 'icon', folder_closed = '📁', folder_open = '📂' },
+          { 'name' },
+        },
+        file = {
+          { 'git_status' },
+          { 'name' },
+        },
+      },
+    },
+    commands = {
+      system_open = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        -- macOS
+        if vim.fn.has 'mac' == 1 then
+          vim.fn.jobstart({ 'open', path }, { detach = true })
+        -- Linux
+        elseif vim.fn.has 'unix' == 1 then
+          vim.fn.jobstart({ 'xdg-open', path }, { detach = true })
+        -- Windows
+        elseif vim.fn.has 'win32' == 1 then
+          local p
+          local lastSlashIndex = path:match '^.+()\\[^\\]*$'
+          if lastSlashIndex then
+            p = path:sub(1, lastSlashIndex - 1)
+          else
+            p = path
+          end
+          vim.cmd('silent !start explorer ' .. p)
+        end
+      end,
     },
   },
 }
